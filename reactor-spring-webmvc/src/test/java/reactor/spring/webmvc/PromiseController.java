749af376e1d109059538d1f21da4bf569db4ed6b
Jon Brisbin <jon@jbrisbin.com>
2014-10-14 08:43:05 -0500

    Update to latest package structure.

--- reactor-spring-webmvc/src/test/java/reactor/spring/webmvc/PromiseController.java
+++ reactor-spring-webmvc/src/test/java/reactor/spring/webmvc/PromiseController.java
@@ -4,12 +4,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import reactor.core.Environment;
 import reactor.core.Reactor;
 import reactor.event.Event;
 import reactor.rx.Promise;
-import reactor.rx.spec.Promises;
+import reactor.rx.Promises;
 
 
 /**
