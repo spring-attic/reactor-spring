4eef9c907349a31dea2a39d9ed7d9a41ba8e5157
Stephane Maldini <smaldini@pivotal.io>
2016-01-26 22:52:51 +0000

    Sync with RSC (zip operator, scalar optimization and package trait split)

--- reactor-spring-core/src/main/java/reactor/spring/core/task/AbstractAsyncTaskExecutor.java
+++ reactor-spring-core/src/main/java/reactor/spring/core/task/AbstractAsyncTaskExecutor.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright (c) 2011-2016 Pivotal Software Inc, All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package reactor.spring.core.task;
 
 import java.util.ArrayList;
@@ -21,8 +36,8 @@ import org.reactivestreams.Subscription;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import reactor.core.publisher.ProcessorExecutor;
+import reactor.core.state.Pausable;
 import reactor.core.timer.Timer;
-import reactor.core.trait.Pausable;
 import reactor.core.util.Exceptions;
 import reactor.fn.Consumer;
 
